b09dc03518e61a39787a9d77e58504f98f8ce732
Sasha Fomina <afomina@stuy.edu>
2018-02-02 17:39:14 -0500

    finished

---
+++ draw.js
@@ -0,0 +1,43 @@
+var canvas = document.getElementById("slate")
+var context = canvas.getContext("2d")
+var b1 = document.getElementById("b1")
+var b2 = document.getElementById("b2")
+var t = 0;
+
+var clear = function(e){
+    context.clearRect(0,0,500,500);
+    
+}
+
+var draw = function(e){
+    var x = e.clientX;
+    var y = e.clientY;
+    console.log(x);
+    console.log(y);
+    context.fillStyle= "black";
+    if (t == 0){
+	context.fillRect(x,y, 20, 20);
+    }
+    else {
+	context.beginPath();
+	context.arc(x,y,10, 0 , 2*Math.PI);
+	context.stroke();
+	context.fill();
+    }
+    
+}
+
+
+var toggle = function(e){
+    if (t== 0) {
+	t = 1;
+    }
+    else {
+	t = 0;
+    }
+    console.log(t);
+}
+
+canvas.addEventListener("click", draw);
+b1.addEventListener("click", clear);
+b2.addEventListener("click", toggle);
